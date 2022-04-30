e0dd726f52a62fe6a2f575af3aae491317465a14
Cesar Torres <cearto@uta.edu>
2022-04-30 12:16:29 -0500

    Route reworking for public release.

--- tool/config/routes.rb
+++ tool/config/routes.rb
@@ -1,16 +1,9 @@
 Rails.application.routes.draw do
-  get 'tool' =>"ebroidery#tool", :as => "tool"
-  get "gallery" => "ebroidery#gallery", :as => "gallery"
-
-  namespace :heater do
-  	get "tool"
-  	get "keys"
-  end
-
   namespace :ebroidery do 
     get "keys"
-    
+    get 'tool', :as => "tool"
+    get "gallery", :as => "gallery"
   end
-  get "motion", action: "motion", controller: "application"
+
   root 'ebroidery#gallery'
 end
